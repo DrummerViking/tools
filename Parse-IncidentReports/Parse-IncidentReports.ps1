07e47fae3e9a2458ef3ed7f7864734ee3101434e
Agustin Gallegos <agallego@microsoft.com>
2022-11-11 18:24:44 -0300

    update script

--- Parse-IncidentReports/Parse-IncidentReports.ps1
+++ Parse-IncidentReports/Parse-IncidentReports.ps1
@@ -22,7 +22,7 @@
 #>
 Param(
     [Parameter(Position = 1, Mandatory = $False, HelpMessage = 'The email address you want the report to be sent to...')]
-    [string]$recipients = '',
+    [string[]]$recipients = '',
     [Parameter(Position = 2, Mandatory = $False, HelpMessage = 'Send report to Organization Admins detected...')]
     [Switch]$OrgAdmins = $False		
 )
@@ -85,7 +85,6 @@ $ClientId = "8799ab60-ace5-4bda-b31f-621c9f6668db"
 $RedirectUri = "http://localhost/code"
 $scopes = New-Object System.Collections.Generic.List[string]
 $scopes.Add("https://outlook.office365.com/.default")
-#$scopes.Add("https://outlook.office.com/EWS.AccessAsUser.All")
 try {
     $token = Get-MsalToken -ClientId $clientID -RedirectUri $RedirectUri -Scopes $scopes -Interactive -ErrorAction Stop
 }
@@ -96,155 +95,144 @@ catch {
         exit
     }
 }
-$exchangeCredentials = New-Object Microsoft.Exchange.WebServices.Data.OAuthCredentials($Token.Result.AccessToken)
+$exchangeCredentials = New-Object Microsoft.Exchange.WebServices.Data.OAuthCredentials($Token.AccessToken)
 $service.Url = New-Object Uri("https://outlook.office365.com/ews/exchange.asmx")
 $Service.Credentials = $exchangeCredentials
 $service.ReturnClientRequestId = $true
-$service.UserAgent = "ParseIncidentReports/1.60"
+$service.UserAgent = "ParseIncidentReports/1.80"
 
-[int]$option = $null
+Function Find-Subfolders {
+    Param (
+        $ParentFolderId,
 
-while ($Option -ne "0") {
-    $Option = $null
-    Write-Host ""
-    Write-Host ""
-    Write-Host "1- List Folders in Root" -ForegroundColor Green
-    Write-Host "2- List Folders in Archive Root" -ForegroundColor Green
-    Write-Host "3- List Folders in Public Folder Root" -ForegroundColor Green
-    Write-Host "4- List subFolders from a desired Parent Folder" -ForegroundColor Green
-    Write-Host "5- Generate Parsed incident reports in a folder to CSV file" -ForegroundColor Green
-    Write-Host "0- To Exit" -ForegroundColor Green
-    $Option = Read-Host -Prompt "Select your number"
+        $ParentDisplayname
+    )
+    $sourceFolderId = new-object Microsoft.Exchange.WebServices.Data.FolderId($ParentFolderId)
+    $service.ClientRequestId = (New-Guid).ToString()
+    $rootfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$sourceFolderId)
 
-    If ($Option -ge "1" -and $Option -le "4") {
-        switch ($option) {
-            1 { $Wellknownfolder = "MsgFolderRoot" }
-            2 { $Wellknownfolder = "ArchiveMsgFolderRoot" }
-            3 { $Wellknownfolder = "PublicFoldersRoot" }
-            4 {
-                Write-Host "Enter folder ID:" -NoNewline
-                $Wellknownfolder = Read-Host
-            }
-        }
-
-        #listing all available folders in the mailbox
-        $FolderView = New-Object Microsoft.Exchange.WebServices.Data.FolderView(100);
-        if ($option -eq "4") {
-            $sourceFolderId = new-object Microsoft.Exchange.WebServices.Data.FolderId($Wellknownfolder)
-            $rootfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service, $sourceFolderId)
-        }
-        else {
-            $rootfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service, [Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::$Wellknownfolder);
-        }
-
-        Write-Host "The '" -nonewline -ForegroundColor DarkYellow; write-host $rootfolder.DisplayName -NoNewline -ForegroundColor DarkYellow ; write-host "' has " -NoNewline -ForegroundColor DarkYellow ; write-host $rootfolder.ChildFolderCount -NoNewline -ForegroundColor DarkYellow ; write-host " child folders." -ForegroundColor DarkYellow
- 
-        $rootfolder.load()
-        foreach ($folder in $rootfolder.FindFolders($FolderView) ) {
-            write-host "Name: " -NoNewline ; write-host $folder.DisplayName -NoNewline ; write-host " , Id: " -NoNewline ; write-host $folder.Id
-        }
-    }
+    $FolderView = New-Object Microsoft.Exchange.WebServices.Data.FolderView(100)
+    #$FolderView.Traversal = "Deep"
     
+    $service.ClientRequestId = (New-Guid).ToString()
+    $rootfolder.load()
+    $service.ClientRequestId = (New-Guid).ToString()
+    foreach ($folder in $rootfolder.FindFolders($FolderView) ) {
+        $DisplayName = "$ParentDisplayname\$($Folder.Displayname)"
+        $output = $folder | Select-Object @{N = "Displayname" ; E = {$DisplayName}}, Id
+        if ($folder.ChildFolderCount -gt 0) {
+            #write-host "looking for subfolders under $($folder.displayname)" -ForegroundColor Green
+            $service.ClientRequestId = (New-Guid).ToString()
+            Find-Subfolders -ParentFolderId $folder.id -ParentDisplayname $Displayname -Array $array
+        }
+        $output
+    }
+}
+$rootfolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service, [Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::MsgFolderRoot)
+$folders = Find-Subfolders -ParentFolderId $rootfolder.id -ParentDisplayname ""
+[int]$index = 0
+$folders | ForEach-Object {
+    [PSCustomObject] @{ Order = $index; DisplayName = $_.DisplayName }; $index++
+} | Out-Host
+Do {
+    Write-Host "Choose the order number for the folder where your Incident reports are located: " -NoNewline
+    [int]$option = Read-Host
+} while ( $option -lt 0 -or $option -ge $folders.Count)
 
-    If ($Option -eq "5") {
-        Write-Host "PLEASE take note of the Folder ID that you need and paste it." -ForegroundColor Yellow -NoNewline        
-        $sourceFolderText = Read-Host " "
-        Write-Host ""
-        
-        $filename = "$env:userprofile\desktop\Incident Report - $((Get-Date).ToString("MM-dd-yyyy HH_mm_ss")).csv"
-        "Received Time, Report Id, Message Id, Sender, Subject, To, Rule Hit" | Out-File $filename -Encoding ascii -Append
-        Write-Host "Report File generated: $filename" -foregroundColor Green
-        Write-Host "Please wait while we work on the e-mail reports" -foregroundColor Green
+$filename = "$env:userprofile\desktop\Incident Report - $((Get-Date).ToString("MM-dd-yyyy HH_mm_ss")).csv"
+"Received Time, Report Id, Message Id, sender, Subject, To, Rule Hit" | Out-File $filename -Encoding ascii -Append
+Write-Host "Report File generated: $filename" -foregroundColor Green
+Write-Host "Please wait while we work on the incidents reports" -foregroundColor Green
 
-        $folderid = new-object Microsoft.Exchange.WebServices.Data.FolderId($sourceFolderText)
-        $Folder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service, $folderid)
+$Folder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service, $folders[$option].Id)
 
-        $ivItemView = New-Object Microsoft.Exchange.WebServices.Data.ItemView(250)  
+$ivItemView = New-Object Microsoft.Exchange.WebServices.Data.ItemView(250)
+$nl = "`n`r"
+$fiItems = $null
+do {  
+    $fiItems = $service.FindItems($Folder.Id, $ivItemView)  
+    foreach ($Item in $fiItems.Items) {  
+        $TempItem = [Microsoft.Exchange.WebServices.Data.Item]::Bind($service, $Item.Id)
+        $text = $TempItem.Body.Text.Replace("<br>", $nl)
+        $text = $text.split($nl)
 
-        $fiItems = $null  
-        do {  
-            $fiItems = $service.FindItems($Folder.Id, $ivItemView)  
-            foreach ($Item in $fiItems.Items) {  
-                $TempItem = [Microsoft.Exchange.WebServices.Data.Item]::Bind($service, $Item.Id)
-                $text = $TempItem.Body.Text.Replace("<br>", $nl)
-                $text = $text.split($nl)
-
-                $ReportId = $text | Select-String -SimpleMatch "Report Id: " | Out-String
-                $MessageID = $text | Select-String -SimpleMatch "Message Id: " | Out-String
-                $sender = $text | Select-String -SimpleMatch "Sender: " | Out-String
-                $Subject = $text | Select-String -SimpleMatch "Subject: " | Out-String
-                $To = $text | Select-String -SimpleMatch "To: " | Out-String
-                $RuleHit = $text | Select-String -SimpleMatch "Rule Hit: " | Out-String
-						
-                $ReportId = $ReportId.TrimEnd()
-                $MessageID = $MessageID.TrimEnd()
-                $sender = $sender.TrimEnd()
-                $Subject = $Subject.TrimEnd()
-                $To = $To.TrimEnd()
-                if ($RuleHit.Contains("$nl")) {
-                    $RuleHit = $RuleHit.Replace("$nl", "")
-                    $RuleHit = $RuleHit.TrimEnd()
-                }
-
-                $ReportId = $ReportId.Substring($ReportId.IndexOf(':') + 2)
-                $MessageID = $MessageID.Substring($MessageID.IndexOf(':') + 2)
-                $MessageID = $MessageID.Replace("&lt;", "<")
-                $MessageID = $MessageID.Replace("&gt;", ">")
-                $sender = $sender.Substring($sender.IndexOf(':') + 2)
-                if ($sender.Contains(",")) {
-                    $sender = $sender.Split(",")[1]
-                }
-                $Subject = $Subject.Substring($Subject.IndexOf(':') + 2)
-                $To = $To.Substring($To.IndexOf(':') + 2)
-                if ($To.Contains(",")) {
-                    $To = $To.Split(",")[1]
-                    $To = $To.TrimStart()
-                    $To = $To.Split("$nl")[0]
-                    $To = $To.TrimEnd()
-                }
-                $RuleHit = $RuleHit.Substring($RuleHit.IndexOf(':') + 2)
-                $RuleHit = $RuleHit.Replace(",", ";")
-                                                     
-                $out = $TempItem.DateTimeReceived.GetDateTimeFormats()[44].toString() + "," + $ReportId + "," + $MessageID + "," + $sender + "," + $Subject + "," + $To + "," + $RuleHit     
-                $out | Out-File $filename -Append -Encoding ascii
-            }  
-            $ivItemView.Offset += $fiItems.Items.Count  
-        }while ($fiItems.MoreAvailable -eq $true)  
-
-        Write-Host "Report File finished." -foregroundColor Green
-
-        if ($recipients -ne '' -or $OrgAdmins -ne $False) {
-            if ($Null -eq $Cred) {
-                $Cred = Get-Credential -Message "Type your Sender's credentials" -UserName $email
-            }
-
-            #region parameters
-            $listrecipients = New-Object System.Collections.ArrayList
-
-            # If Switch $OrgAdmins is in use, we will check current admins and include them to the recipients list
-            if ($OrgAdmins -eq $True) {
-                $endpoint = "outlook.office365.com/powershell-liveid/"
-                $Session = New-PSSession -Name EXO -ConfigurationName Microsoft.Exchange -ConnectionUri https://$endpoint -Authentication Basic -AllowRedirection -Credential $cred
-                Import-PSSession $Session -AllowClobber -CommandName Get-RoleGroupMember, Get-Mailbox | Out-Null
+        $ReportId = $text | Select-String -SimpleMatch "Report Id: " | Out-String
+        $MessageID = $text | Select-String -SimpleMatch "Message Id: " | Out-String
+        $senderAddress = $text | Select-String -SimpleMatch "Sender: " | Out-String
+        $Subject = $text | Select-String -SimpleMatch "Subject: " | Out-String
+        $To = $text | Select-String -SimpleMatch "To: " | Out-String
+        $RuleHit = $text | Select-String -SimpleMatch "Rule Hit: " | Out-String
                 
-                $TenantAdmins = Get-RoleGroupMember "Organization Management"
-                foreach ($admin in (Get-RoleGroupMember $TenantAdmins.Name)) {
-                    if ($Recipients -ne '') {
-                        $Recipients = $Recipients + ", "
-                    }
-                    $Recipients = $Recipients + (Get-Mailbox $admin.Name).PrimarySmtpAddress
-                }
+        $ReportId = $ReportId.TrimEnd()
+        $MessageID = $MessageID.TrimEnd()
+        $senderAddress = $senderAddress.TrimEnd()
+        $Subject = $Subject.TrimEnd()
+        $To = $To.TrimStart() ; $To = $To.TrimEnd()
+        if ($RuleHit.Contains("$nl")) {
+            $RuleHit = $RuleHit.Replace("$nl", "")
+            $RuleHit = $RuleHit.TrimEnd()
+        }
+
+        $ReportId = $ReportId.Substring($ReportId.IndexOf('Report Id:') + 11)
+        $MessageID = $MessageID.Substring($MessageID.IndexOf(':') + 2)
+        $MessageID = $MessageID.Replace("&lt;", "<")
+        $MessageID = $MessageID.Replace("&gt;", ">")
+        $senderAddress = $senderAddress.Substring($senderAddress.IndexOf(':') + 2)
+        if ($senderAddress.Contains(",")) {
+            $senderAddress = $senderAddress.Split(",")[1]
+        }
+        $Subject = $Subject.Substring($Subject.IndexOf(':') + 2)
+        $to -match "[\w-\.]+@([\w-]+\.)+[\w-]{2,4}" | Out-Null
+        $to = $Matches.Values -join ";"
+        <#
+        $To = $To.Substring($To.IndexOf(':') + 2)
+        if ($To.Contains(",")) {
+            $To = $To.Split(",")[1]
+            $To = $To.TrimStart()
+            $To = $To.Split("$nl")[0]
+            $To = $To.TrimEnd()
+        }
+        #>
+        $RuleHit = $RuleHit.Replace("Rule Hit: ","")
+        $RuleHit = $RuleHit.Replace(",", ";")
+                                                
+        $out = $TempItem.DateTimeReceived.GetDateTimeFormats()[14].toString() + "," + $ReportId + "," + $MessageID + "," + $senderAddress + "," + $Subject + "," + $To + "," + $RuleHit     
+        $out | Out-File $filename -Append -Encoding ascii
+    }  
+    $ivItemView.Offset += $fiItems.Items.Count  
+}while ($fiItems.MoreAvailable -eq $true)  
+
+Write-Host "Report File finished." -foregroundColor Green
+
+if ($recipients -ne '' -or $OrgAdmins -ne $False) {
+    #region parameters
+    $listrecipients = New-Object System.Collections.ArrayList
+
+    # If Switch $OrgAdmins is in use, we will check current admins and include them to the recipients list
+    if ($OrgAdmins -eq $True) {
+        if ( -not(Get-Module ExchangeOnlineManagement -ListAvailable )) {
+            Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force
+        }
+        if (-not(Get-ConnectionInformation) ) {
+            $script:cred = Get-Credential
+            Connect-ExchangeOnline -Credential $script:cred -ShowBanner:$false
+        }
+        
+        $TenantAdmins = Get-RoleGroupMember "Organization Management"
+        foreach ($admin in (Get-RoleGroupMember $TenantAdmins.Name)) {
+            if ($Recipients -ne '') {
+                $Recipients = $Recipients + ", "
             }
-            $listrecipients = ("$Recipients").Split(",")
-            Write-Host "Sending Report by e-mail to:" $recipients -ForegroundColor Yellow
-            Write-Host
-            #endregion parameters
-
-
-            # sending message
-
-            $Subject = "Incident Report - $((Get-Date).ToString("yyyy-MM-dd HH:mm:ss"))"
-            Send-MailMessage -From $cred.UserName -To $listrecipients -Body "Incident report generated on $((Get-Date).ToString("MM/dd/yyyy HH:mm:ss"))" -SmtpServer smtp.office365.com -UseSsl -Port 587 -Subject $Subject -Credential $cred -Attachments $filename
+            $Recipients = $Recipients + (Get-Mailbox $admin.Name).PrimarySmtpAddress
         }
     }
+    $listrecipients = ("$Recipients").Split(",")
+    Write-Host "Sending Report by e-mail to:" $recipients -ForegroundColor Yellow
+    Write-Host
+    #endregion parameters
+
+
+    # sending message
+    $Subject = "Incident Report - $((Get-Date).ToString("yyyy-MM-dd HH:mm:ss"))"
+    Send-MailMessage -From $cred.UserName -To $listrecipients -Body "Incident report generated on $((Get-Date).ToString("MM/dd/yyyy HH:mm:ss"))" -SmtpServer smtp.office365.com -UseSsl -Port 587 -Subject $Subject -Credential $script:cred -Attachments $filename
 }
